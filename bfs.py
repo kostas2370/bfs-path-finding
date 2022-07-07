ec82e5ecfde9970444fe0b5dc372626730c1b918
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2022-07-07 13:47:04 +0300

    bfs2

--- bfs.py
+++ bfs.py
@@ -2,16 +2,16 @@
 #o = discovered
 
 import numpy as np
-
+import pprint
         #0   1   2   3   4   5   6   7   8   9
 map_1=[
        ['-','-','-','-','-','-','-','-','-','-'], #0
-       ['-','-','e','-','e','-','-','-','-','-'], #1
+       ['-','-','e','e','e','e','e','e','e','e'], #1
        ['-','-','-','e','-','-','-','-','-','-'], #2
-       ['-','-','-','-','-','-','-','-','-','-'], #3
-       ['-','-','-','-','e','-','-','-','x','-'], #4
+       ['-','-','e','-','-','e','e','-','e','e'], #3
+       ['-','-','e','-','e','x','e','-','-','-'], #4
        ['-','-','-','-','-','-','e','e','e','-'], #5
-       ['-','-','-','-','-','-','-','-','e','-'], #6
+       ['-','-','e','-','-','-','-','-','e','-'], #6
        ['-','-','-','-','-','-','-','-','e','*']  #7
        ]
 def get_start(mapx):
@@ -19,6 +19,20 @@ def get_start(mapx):
         for j in range (len(mapx[0])):
             if mapx[i][j]=='*':
                 return i,j
+def find_path(maps, start, end, path=[]):
+        path = path + [start]
+        
+        if start == end:
+            return path
+        if start not in maps:
+            return None
+        
+        for node in maps[start]:
+            if node not in path:
+               
+                newpath = find_path(maps, node, end, path)
+                if newpath: return newpath
+        return None
 
 def get_neighbours(mapx,c):
     neighbours={}
@@ -37,34 +51,40 @@ def get_neighbours(mapx,c):
 
 def bfs(st,mapx):
     queue=[]
-    close=[]
-    result=[]
+    lista=[]
+    x={}
+    for j in get_neighbours(mapx,st): lista.append(get_neighbours(mapx,st)[j])
+        
+    x[st]=set(lista)
+    mapx[st[0]][st[1]]='o'
     if (mapx[st[0]][st[1]]=='x'):
         return "first"
-    for y in get_neighbours(mapx,st):
-        queue.append(get_neighbours(mapx,st)[y])
+    for y in get_neighbours(mapx,st):queue.append(get_neighbours(mapx,st)[y])
     while len(queue)!=0:
-        
+        lista =[]  
         current=queue.pop(0)
-
-        close.append(current)
-        if mapx[current[0]][current[1]] =="x":
-          
-            print("Found !")
-            return close
         z=get_neighbours(mapx,current)
-        for p in z:
-            if z[p] not in queue and z[p]  not in close:
-                
-                queue.append(get_neighbours(mapx,current)[p])
-        print (f"{np.matrix(mapx)}\n \n")
-        
-        mapx[current[0]][current[1]]="o"
+        if mapx[current[0]][current[1]] =="x":
+            for p in get_neighbours(mapx,current):lista.append(get_neighbours(mapx,current)[p])
+            x[current]=set(lista)
+            j=find_path(x,st,current)
+            return j
 
+        for p in z:
+            if z[p] not in queue and z[p]  not in x:
+                queue.append(get_neighbours(mapx,current)[p])
+                lista.append(get_neighbours(mapx,current)[p])
+        print(f"{np.matrix(mapx)} \n \n")
+
+        x[current]=set(lista)
+        mapx[current[0]][current[1]]="o"
         
-    print("queue :")
-    return len(queue)
+    return "not in list"
 
 current=get_start(map_1)
 
 print(bfs(current,map_1))
+
+
+
+    
